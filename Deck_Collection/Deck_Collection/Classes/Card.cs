ac393f7208b957645826949d7cbea2c2f1294621
Calamario <mario.nishio@gmail.com>
2018-06-15 06:23:38 -0700

    added readme and gitignore,

---
+++ Deck_Collection/Deck_Collection/Classes/Card.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Deck_Collection.Classes
+{
+    public class Card
+    {
+
+        public Value Value { get; set; }
+
+        public Suit Suit { get; set; }
+    }
+
+    public enum Value
+    {
+        Ace,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
+    }
+
+    public enum Suit
+    {
+        Heart,
+        Spade,
+        Club,
+        Diamond
+    }
+}
---
+++ Deck_Collection/Deck_Collection/Classes/Deck.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Deck_Collection.Classes
+{
+    public class Deck<T> : IEnumerable
+    {
+        public T[] cards = new T[5];
+
+        private int count;
+
+        /// <summary>
+        /// Adds a card to the array
+        /// </summary>
+        /// <param name="card"> Type T card object </param>
+        public void Add(T card)
+        {
+            // make the array bigger if the array is full
+            if (count == cards.Length)
+            {
+                Array.Resize(ref cards, cards.Length * 2);
+            }
+
+            // add the card obj into the array
+            cards[count++] = card;
+        }
+
+        /// <summary>
+        /// Logic Incomplete
+        /// </summary>
+        /// <param name="card"></param>
+        public void RemoveCard(T card)
+        {
+
+        }
+
+        /// <summary>
+        /// Removes all the cards in the array
+        /// </summary>
+        public void RemoveExpensive()
+        {
+            Array.Resize(ref cards, 0);
+            Array.Resize(ref cards, 5);
+        }
+
+        /// <summary>
+        /// Shuffles the current list of cards
+        /// </summary>
+        public void Shuffle()
+        {
+            Random rng = new Random();
+            int cardNum = cards.Length;
+            while (cardNum > 1)
+            {
+                //Decrements the total number of cards so that each position will change places with another
+                int randomNum = rng.Next(0, cardNum--);
+                T temp = cards[cardNum];
+                cards[cardNum] = cards[randomNum];
+                cards[randomNum] = temp;
+            }
+
+        }
+
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return cards[i];
+            }
+        }
+
+        // magic 
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
